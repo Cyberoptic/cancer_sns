a76e044fe695cca4865bc497e701f6d8f80047e8
Takehiro Mouri <takehiromouri@gmail.com>
2016-12-20 00:10:34 +0900

    prevent extra files from generating

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,17 @@ module CancerSns
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.default_locale = :ja
+
+    config.active_record.time_zone_aware_types = [:datetime, :time]
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
+    config.generators do |g|
+		  g.helper false
+		  g.assets false
+		end
   end
 end
