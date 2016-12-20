9dbb52f282e0e5d830d82fd0145670efe4dee9bc
Srijal Joshi <bladedge666@gmail.com>
2016-12-20 15:09:33 +0545

    Omniauth facebook pulling required data

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,6 +247,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: "http://localhost:3000/users/auth/facebook/callback"
+
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   # ==> Warden configuration
