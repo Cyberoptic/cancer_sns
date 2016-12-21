73e28c74636c516fa23959b95a68e07a2c419642
Srijal Joshi <bladedge666@gmail.com>
2016-12-21 22:20:48 +0545

    Add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :trackable, :validatable,
   :omniauthable, :omniauth_providers => [:facebook]
 
+  devise :registerable, :confirmable
+  
   enum gender: {female: 0, male: 1, other: 2}
 
   validates :first_name, :last_name, :first_name_katakana, :last_name_katakana, :birthday, :gender, :email, :partner_age, :cancer_type, :cancer_stage, :treatment, presence: true
