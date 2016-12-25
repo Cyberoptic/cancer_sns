1245bc32cddd8e280c63da57e755c253c036bd6f
Srijal Joshi <bladedge666@gmail.com>
2016-12-25 17:08:00 +0545

    Cant be blank error not allowing post to be saved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,38 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @posts = Post.newest_first
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @post_attachments = @post.post_attachments.all   
+  end
+
+  def new
+    @post = Post.new
+    @post_image = @post.post_images.build
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        params[:post_images]['photo'].each do |a|
+          @post_image = @post.post_images.create!(:photo => a)
+        end
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :content, post_images_attributes: [:id, :user_id, :post_id, :photo])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :post_images #dependent destroy
+  accepts_nested_attributes_for :post_images
+
   validates :content, :user, presence: true
+
+  scope :newest_first, ->{order(created_at: :desc)}
 end
