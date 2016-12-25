3e230152d5b5e27467dee32812294b3cc6ff1ca8
Takehiro Mouri <takehiromouri@gmail.com>
2016-12-25 15:38:09 +0900

    add test for friendships controller

--- app/controllers/friend/request_acceptances_controller.rb
+++ app/controllers/friend/request_acceptances_controller.rb
@@ -1,21 +1,21 @@
 class Friend::RequestAcceptancesController < ApplicationController
-    
-    def create
-        @user = User.find(params[:user_id])
-        current_user.accept_request(@user)
-        respond_to do |format|
-          format.html {}
-          format.js {}
-        end   
-    end
+  before_action :authenticate_user!
 
-    def destroy
-        @user = User.find(params[:user_id])
-        current_user.decline_request(@user)
-        respond_to do |format|
-          format.html {}
-          format.js {}
-        end    
-    end
+  def create
+    @user = User.find(params[:user_id])
+    current_user.accept_request(@user)
+    respond_to do |format|
+      format.html {}
+      format.js {}
+    end   
+  end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    current_user.decline_request(@user)
+    respond_to do |format|
+      format.html {}
+      format.js {}
+    end    
+  end
 end
--- spec/controllers/friend/request_acceptances_controller_spec.rb
+++ spec/controllers/friend/request_acceptances_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Friend::RequestAcceptancesController, type: :controller do
+	describe "POST #create" do
+		context "when user is signed in" do
+			it "should accept the friend request" do
+				user = create(:user)
+				other_user = create(:user)
+				other_user.friend_request(user)
+				allow(user).to receive(:accept_request).with(other_user)
 
+				post :create, params: { user_id: other_user }
+
+				expect(user).to have_received(:accept_request)
+			end
+			
+		end
+	end
 end
