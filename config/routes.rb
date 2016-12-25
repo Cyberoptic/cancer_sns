3008c4ad3879c351b5857e4e76ab9b02c2720c3f
Srijal Joshi <bladedge666@gmail.com>
2016-12-25 21:31:26 +0545

    Merge conflicts fix final

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'static#home'
 
   resources :posts
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
+
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "registrations" }
   
   resources :users, only: :index
