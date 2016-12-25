f4261ed1f51c0f74641c20213b4689f0c4ef49ea
Srijal Joshi <bladedge666@gmail.com>
2016-12-25 21:29:34 +0545

    Resolve merge conflicts of routes.rb and app.h.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   root 'static#home'
-<<<<<<< HEAD
+
   resources :posts
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-
+  
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "registrations" }
   
   resources :users, only: :index
->>>>>>> 95181f0bbb41ff422e65efb432457a1dba77648d
 end
