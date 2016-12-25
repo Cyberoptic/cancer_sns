04c103495f75369355606b536fc38026d8324c4c
Takehiro Mouri <takehiromouri@gmail.com>
2016-12-25 19:25:25 +0900

    disable requirement of current_password on user edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static#home'
 
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "registrations" }
   
   resources :users, only: :index
 end
---
+++ spec/decorators/registration_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+describe RegistrationDecorator do
+end
