41a890b63984aa046512f3fc16e5912e4c01e477
Takehiro Mouri <takehiromouri@gmail.com>
2016-12-26 18:56:29 +0900

    tweak front end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 	def index
 		if params[:search]			
-			@users = User.is_public.filter(params[:search].slice(:profession, :partner_age, :cancer_type, :cancer_stage, :hospital, :treatment, :birthday))			
+			@users = User.is_public.filter(params[:search].slice(:profession, :partner_age, :cancer_type, :cancer_stage, :hospital, :treatment, :birthday)).decorate	
 		else
-			@users = User.is_public.all
+			@users = User.is_public.decorate
 		end
 	end
 end
