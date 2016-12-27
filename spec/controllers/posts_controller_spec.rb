3ae3f99b1e241d2a8300a5fa94b730ead586403c
Nischal Gautam <nischal1024@gmail.com>
2016-12-27 20:20:56 +0545

    init testing new and index

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
+require 'spec_helper'
+require 'rspec/rails'
+require 'devise'
 
 RSpec.describe PostsController, type: :controller do
+
+  describe 'GET #index' do
+    it 'render index page' do
+      user = create(:user)
+      puts user.email
+      sign_in user
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'renders new page' do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
 
 end
