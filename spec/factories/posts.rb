77c771f24939789efb9573cdaad104aa9fd9c860
Nischal Gautam <nischal1024@gmail.com>
2016-12-28 09:05:45 +0545

    spec file for post_images

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :post do
     content 'Hello there this is a content'
-    photo Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/images/1.png')))
-
   end
 end
