e6a5a64817ad0076265f602a029cbb8b74ec789d
Srijal Joshi <bladedge666@gmail.com>
2016-12-28 15:36:26 +0545

    Setup like, happy, sad model

--- app/models/sad.rb
+++ app/models/sad.rb
@@ -1,4 +1,8 @@
 class Sad < ApplicationRecord
-	belongs_to :user
-	belongs_to :post
+	belongs_to :user, dependent: :destroy
+	belongs_to :post, dependent: :destroy
+
+	validates :user_id, :post_id, presence: true
+
+	validates :user_id, uniqueness: {scope: :post_id}
 end
