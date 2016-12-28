884def78550ac7ccb01b8f337a6f9158cff37275
Nischal Gautam <nischalgautam@Nischals-MBP.gghaggs>
2016-12-28 22:01:11 +0545

    signin user test pass

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
+  describe '#create user ' do
+    it 'creates new user ' do
+      expect {post :create, user: FactoryGirl.attributes_for(:user)}.to change(User,:count).by(1)
+    end
+  end
 
 end
