8df916444cf0db15d0fb37a6384fd292707c12e0
Takehiro Mouri <takehiromouri@gmail.com>
2016-12-31 01:20:11 +0900

    fix specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -8,79 +8,104 @@ RSpec.describe PostsController, type: :controller do
   describe 'GET #index' do
     it 'render index page' do
       user = create(:user)
-      puts user.email
+
       sign_in user
       get :index
+
       expect(response).to render_template(:index)
     end
   end
 
   describe 'GET #new' do
     it 'renders new page' do
-      user = FactoryGirl.create(:user)
+      user = create(:user)
+
       sign_in user
       get :new
+
       expect(response).to render_template(:new)
     end
   end
 
   describe 'GET #show' do
     it 'renders show page' do
-      user = FactoryGirl.create(:user)
-      sign_in user
-      post = FactoryGirl.create(:post, user_id: user.id)
+      user = create(:user)
+      post = create(:post, user_id: user.id)
+
+      sign_in user      
       get :show, id: post.id
+
       expect(response).to render_template(:show)
     end
   end
 
   describe 'GET #edit' do
     it 'renders edit page' do
-      user = FactoryGirl.create(:user)
+      user = create(:user)
+      post = create(:post, user_id: user.id)
+
       sign_in user
-      post = FactoryGirl.create(:post, user_id: user.id)
       get :edit, id: post.id
+
+      expect(response).to render_template(:edit)
     end
   end
 
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'creates new post' do
-        user = FactoryGirl.create(:user)
+        user = create(:user)
+
         sign_in user
-        expect {post :create, post: FactoryGirl.attributes_for(:post, user_id: user.id)}.to change(Post, :count).by(1)
+
+        expect {
+          post :create, post: attributes_for(:post, user_id: user.id)
+        }.to change(Post, :count).by(1)
       end
       it 'redirects to show page' do
-        user = FactoryGirl.create(:user)
+        user = create(:user)
+
         sign_in user
-        post :create, post: FactoryGirl.attributes_for(:post)
-        expect(response).to redirect_to post_path(assigns[:post])
+        post :create, post: attributes_for(:post)
+
+        expect(response).to redirect_to post_path(assigns[:post].id)
       end
     end
-    context 'with invalidd attributes ' do
+    context 'with invalidd attributes' do
       it 'doesnt create a new post' do
-        user = FactoryGirl.create(:user)
+        user = create(:user)
+
         sign_in user
-        expect {post :create, post: FactoryGirl.attributes_for(:post, content: nil, user_id: user.id)}.to change(Post, :count).by(0)
+
+        expect {
+          post :create, post: attributes_for(:post, content: nil, user_id: user.id)
+        }.to change(Post, :count).by(0)
         expect(response).to render_template :new
       end
     end
   end
 
-  describe ' PUT #update' do
+  describe 'PUT #update' do
     context 'with valid attributes' do
       it 'updates post' do
         user = create(:user)
-        post = create(:post,user_id: user.id)
-        put :update,id: post.id,post: FactoryGirl.attributes_for(:post,content: "hello there")
+        post = create(:post, user_id: user.id)
+
+        sign_in user
+        put :update, id: post.id, post: attributes_for(:post, content: "hello there")
         post.reload
+
         expect(post.content).to eq("hello there")
       end
+
       it 'redirects to show path' do
         user = create(:user)
         post = create(:post,user_id: user.id)
-        put :update, id: post.id, post: FactoryGirl.attributes_for(:post, content: "hello there")
+
+        sign_in user
+        put :update, id: post.id, post: attributes_for(:post, content: "hello there")
         post.reload
+
         expect(response).to redirect_to post_path(assigns[:post])
       end
     end
@@ -88,23 +113,34 @@ RSpec.describe PostsController, type: :controller do
       it 'renders edit' do
         user = create(:user)
         post = create(:post, user_id: user.id)
-        put :update,id: post.id, post: FactoryGirl.attributes_for(:invalid_post)
-        post.reload
+
+        sign_in user
+        put :update,id: post.id, post: attributes_for(:invalid_post)        
+
         expect(response).to render_template :edit
       end
     end
   end
 
-  describe ' Delete #destroy' do
+  describe 'DELETE #destroy' do
     it 'deletes post' do
       user = create(:user)
-      post=create(:post,user_id: user.id)
-      expect {delete :destroy,id: post.id}.to change(Post,:count).by(-1)
+      post = create(:post, user_id: user.id)
+
+      sign_in user
+
+      expect {
+        delete :destroy, id: post.id
+      }.to change(Post,:count).by(-1)
     end
+    
     it 'redirects to posts path' do
-      user= create(:user)
-      post=create(:post,user_id: user.id)
+      user = create(:user)
+      post = create(:post,user_id: user.id)
+
+      sign_in user
       delete :destroy, id: post.id
+
       expect(response).to redirect_to posts_path
     end
   end
