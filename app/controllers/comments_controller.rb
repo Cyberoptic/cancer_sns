b17a4dd2d7923748b69df16066c6d04ff48b7a09
Live session user <ikushum@gmail.com>
2017-01-01 16:04:44 +0545

    fixed bugs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,6 @@ class CommentsController < ApplicationController
 def create
   @post = Post.find(params[:post_id])
   @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
-  if @comment.valid?
-    redirect_to root_path
-  else
-    flash[:alert] = "Invalid attributes."
-    redirect_to root_path
-  end
     respond_to do |format|
       format.html {}
       format.js {}
