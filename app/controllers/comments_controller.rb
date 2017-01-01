f63bd481dda2b0c5d2149ed28e151c4621d6a007
Takehiro Mouri <takehiromouri@gmail.com>
2017-01-01 23:16:55 +0900

    add comment front end

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,10 @@ class CommentsController < ApplicationController
 def create
   @post = Post.find(params[:post_id])
   @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
-    respond_to do |format|
-      format.html {}
-      format.js {}
-    end  
+  respond_to do |format|
+    format.html {}
+    format.js {}
+  end  
 end
 
 private 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-	default_scope { order(created_at: :desc) }
+	default_scope { order(created_at: :asc) }
 
 	validates :text, :user_id, :post_id, presence: true
 	belongs_to :user
 	belongs_to :post
 
-	delegate :display_name, :photo, to: :user, prefix: true
+	delegate :photo, to: :user, prefix: true
 	delegate :user, to: :post, prefix: true	
 
 end
