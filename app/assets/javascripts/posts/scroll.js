ec191051edb4c26d279cbc82c115f8b7a28b0b20
Takehiro Mouri <takehiromouri@gmail.com>
2017-01-09 13:18:34 +0900

    remove comments

--- app/assets/javascripts/posts/scroll.js
+++ app/assets/javascripts/posts/scroll.js
@@ -10,21 +10,3 @@ $(document).ready(function() {
     return $(window).scroll();
   }
 });
-
-
-// $(document).ready(function () {
-//   var isLoading = false;
-//   if ($('#infinite-scrolling').size() > 0) {
-//     $(window).on('scroll', function() {
-//       var more_posts_url = $('.pagination a.next_page').attr('href');
-//       if (!isLoading && more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
-//         isLoading = true;
-//         $.getScript(more_posts_url).done(function (data,textStatus,jqxhr) {
-//           isLoading = false;
-//         }).fail(function() {
-//           isLoading = false;
-//         });
-//       }
-//     });
-//   }
-// });
