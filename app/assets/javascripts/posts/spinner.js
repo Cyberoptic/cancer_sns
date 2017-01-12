d30e17ce30bd1387ef744b16f50b43e2a08dc35d
Srijal Joshi <bladedge666@gmail.com>
2017-01-12 10:46:14 +0545

    Add spinner style and js

---
+++ app/assets/javascripts/posts/spinner.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+  // hide spinner
+  $(".spinner").hide();
+
+
+  // show spinner on AJAX start
+  $(document).ajaxStart(function(){
+    $(".spinner").show();
+  });
+
+  // hide spinner on AJAX stop
+  $(document).ajaxStop(function(){
+    // $(".spinner").hide();
+    
+    $(".spinner").delay(3000).hide(0); // for development
+
+  });
+
+});
