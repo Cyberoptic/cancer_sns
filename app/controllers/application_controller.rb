489246239a33497e3608d5c2e5e81e0cc9eb5feb
Takehiro Mouri <takehiromouri@gmail.com>
2017-01-24 10:40:17 +0545

    add strong params to user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :provider, :uid) }
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:nickname, :email, :password, :password_confirmation, :photo, :first_name, :last_name, :first_name_katakana, :last_name_katakana, :birthday, :gender, :email, :partner_age, :cancer_type, :cancer_stage, :treatment, :area, :treatment, :profession, :hospital, :problems, :profile_completed, :is_public, :show_name, :show_profession, :show_partner_age, :show_cancer_type, :show_cancer_stage, :show_hospital, :show_treatment, :show_birthday, :show_problems, :show_area) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:nickname, :email, :password, :password_confirmation, :photo, :first_name, :last_name, :first_name_katakana, :last_name_katakana, :birthday, :gender, :email, :partner_age, :cancer_type, :cancer_stage, :treatment, :area, :treatment,:treatment_content, :prefecture, :profession, :hospital, :problems, :profile_completed, :is_public, :show_name, :show_profession, :show_partner_age, :show_cancer_type, :show_cancer_stage, :show_hospital, :show_treatment, :show_birthday, :show_problems, :show_area) }
   end
 
 end
