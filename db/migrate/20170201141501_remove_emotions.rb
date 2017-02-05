bed6e55cef685a43c54574c220790174e9bac9b7
Takehiro Mouri <takehiromouri@gmail.com>
2017-02-06 00:40:39 +0545

    fix migrations

--- db/migrate/20170201141501_remove_emotions.rb
+++ db/migrate/20170201141501_remove_emotions.rb
@@ -1,8 +1,8 @@
 class RemoveEmotions < ActiveRecord::Migration[5.0]
   def change
-    drop_table :happies
-    drop_table :sads
-    drop_table :likes
+    drop_table :happies unless table_exists? :happies
+    drop_table :sads unless table_exists? :sads
+    drop_table :likes unless table_exists? :likes
     remove_column :posts, :happies_count
     remove_column :posts, :likes_count
     remove_column :posts, :sads_count
