7ecce0e6bd69f42f383219971ccca66df5c17363
Takehiro Mouri <takehiromouri@gmail.com>
2017-02-06 20:36:48 +0545

    remove comment

--- app/models/user_treatment.rb
+++ app/models/user_treatment.rb
@@ -1,8 +1,6 @@
 class UserTreatment < ApplicationRecord
   belongs_to :user
   belongs_to :treatment
-
-  # validates :user_id, uniqueness: {scope: :treatment_id}
   
   accepts_nested_attributes_for :treatment
 end
