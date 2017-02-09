ec5b8750faac5b531470e2d5b03ea80fff3f5646
Srijal Joshi <bladedge666@gmail.com>
2017-02-09 16:32:50 +0545

    Groups#show specs

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -15,7 +15,14 @@ RSpec.describe GroupsController, type: :controller do
 
   describe "GET #show" do
     it "renders a specific group's page(show template)" do
-
+			# setup
+			user = create(:user)
+			group = create(:group)
+			# exercise
+			sign_in user
+			get :show, id: group.id
+			# verify
+			expect(response).to render_template(:show)
     end
   end
 end
