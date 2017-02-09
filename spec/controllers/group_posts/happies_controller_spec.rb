1ccc401a942b0889446e5d753e27244b117697ba
Srijal Joshi <bladedge666@gmail.com>
2017-02-10 00:46:27 +0545

    GroupPosts::Happies#create spec

--- spec/controllers/group_posts/happies_controller_spec.rb
+++ spec/controllers/group_posts/happies_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe GroupPosts::HappiesController, type: :controller do
-
+  describe "POST #create" do
+    context "when attriutes are valid" do
+      it "triggers #happy" do
+        # setup
+        user = create(:user)
+        group = create(:group)
+        group_post = create(:group_post, user_id: user.id, group_id: group.id)
+        allow(controller).to receive(:current_user).and_return(user)
+        allow(user).to receive(:happy).with(group_post).and_return(true)
+        # exercise
+        binding.pry
+        sign_in user
+        post :create, params: { group_post_id: group_post.id }, format: :js
+        # verify
+        expect(controller.current_user).to have_received(:happy)
+      end
+    end
+  end
 end
