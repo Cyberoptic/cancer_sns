14ebda8ed91df3120c393968f1941fc422af0aac
Takehiro Mouri <takehiromouri@gmail.com>
2017-02-13 13:36:59 +0545

    add specs

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_moderator!, only: [:edit, :update]
 
   def index
     @my_groups = current_user.groups
@@ -13,7 +14,7 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @group = Group.find(params[:id])
+    @group = find_group
     @posts = @group.group_posts.includes(:user, :post_images).paginate(page: params[:page], per_page: 5).decorate
     @post = GroupPost.new
     @post_images = @post.post_images.build
@@ -40,9 +41,42 @@ class GroupsController < ApplicationController
       flash[:alert] = @group.errors.full_messages[0]
       render :new
     end
-  end  
+  end
+
+  def edit
+    @group = find_group
+    @unread_messages = Message.unread_by(current_user).includes(chat_room: [:user, :member, :messages])
+    @unread_group_posts =  GroupPost.unread_by(current_user).includes(:user, :group)
+  end
+
+  def update
+    @group = find_group
+    if @group.update(group_params)
+      flash[:success] = "グループ情報が更新されました。"
+      redirect_to group_path(@group)
+    else
+      flash[:alert] = @group.errors.full_messages[0]
+
+      @unread_messages = Message.unread_by(current_user).includes(chat_room: [:user, :member, :messages])
+      @unread_group_posts =  GroupPost.unread_by(current_user).includes(:user, :group)
+      
+      render :edit
+    end
+  end
 
   private
+
+  def find_group
+    @group ||= Group.find(params[:id])
+  end
+
+  def ensure_moderator!
+    @group = find_group
+    group_membership = current_user.group_memberships.find_by(group: @group)
+    return if group_membership.moderator?
+    flash[:alert] = "グループ管理者以外編集できません。"
+    redirect_to group_path(@group)
+  end
 
   def mark_posts_as_read
     @group.group_posts.mark_as_read! :all, for: current_user
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -39,4 +39,99 @@ RSpec.describe GroupsController, type: :controller do
       end
     end
   end
+
+  describe "GET #edit" do
+    context "when user is moderator" do
+      it "renders the edit template" do
+        user = create(:user)
+        group = create(:group)
+        create(:group_membership, user: user, group: group, role: :moderator)
+
+        sign_in user
+        get :edit, params: {id: group.id}
+
+        expect(response).to render_template :edit
+      end
+    end
+
+    context "when user is not moderator" do
+      it "redirects to group_path" do
+        user = create(:user)
+        group = create(:group)
+        create(:group_membership, user: user, group: group, role: :member)
+
+        sign_in user
+        get :edit, params: {id: group.id}
+
+        expect(response).to redirect_to group_path(group)
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "when user is moderator" do
+      context "when attributes are valid" do
+        it "updates the group information" do
+          user = create(:user)
+          group = create(:group, name: "Group")
+          create(:group_membership, user: user, group: group, role: :moderator)
+
+          sign_in user
+          put :update, params: {id: group.id, group: {name: "New Group Name"}}
+          group.reload
+
+          expect(group.name).to eq("New Group Name")
+        end
+
+        it "redirects to group_path" do
+          user = create(:user)
+          group = create(:group, name: "Group")
+          create(:group_membership, user: user, group: group, role: :moderator)
+
+          sign_in user
+          put :update, params: {id: group.id, group: {name: "New Group Name"}}
+
+          expect(response).to redirect_to group_path(group)
+        end
+      end
+
+      context "when attributes are invalid" do
+        it "does not update the group information" do
+          user = create(:user)
+          group = create(:group, name: "Group")
+          create(:group_membership, user: user, group: group, role: :moderator)
+
+          sign_in user
+          put :update, params: {id: group.id, group: {name: nil}}
+          group.reload
+
+          expect(group.name).to eq("Group")
+        end
+
+        it "renders the edit template" do
+          user = create(:user)
+          group = create(:group, name: "Group")
+          create(:group_membership, user: user, group: group, role: :moderator)
+
+          sign_in user
+          put :update, params: {id: group.id, group: {name: nil}}
+          
+          expect(response).to render_template :edit
+        end
+      end
+    end
+
+    context "when user is not moderator" do
+      it "redirects to group_path" do
+        user = create(:user)
+        group = create(:group)
+        create(:group_membership, user: user, group: group, role: :member)
+
+        sign_in user
+        put :update, params: {id: group.id, group: {name: nil}}
+
+        expect(response).to redirect_to group_path(group)
+      end
+    end
+  end
 end
