d1ee440cbcc984ae4f57e483f32c349134caea85
Takehiro Mouri <takehiromouri@gmail.com>
2017-02-19 17:59:22 +0545

    add next page to paginating decorator

--- app/decorators/paginating_decorator.rb
+++ app/decorators/paginating_decorator.rb
@@ -1,6 +1,6 @@
 class PaginatingDecorator < Draper::CollectionDecorator
 	delegate :current_page, :total_pages, :limit_value, :total_count,
-     :entry_name, :offset_value, :last_page?, :total_entries
+     :entry_name, :offset_value, :last_page?, :total_entries, :next_page
 
   def entry_name
     object.class.name
