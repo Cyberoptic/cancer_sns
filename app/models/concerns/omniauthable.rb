a441b9805d86b190f19039ca430bf41f40887d45
Takehiro Mouri <takehiromouri@gmail.com>
2017-02-23 16:22:19 +0545

    move omniauthable out

--- app/models/concerns/omniauthable.rb
+++ app/models/concerns/omniauthable.rb
@@ -2,28 +2,6 @@ module Omniauthable
   extend ActiveSupport::Concern 
 
   module ClassMethods
-    def new_with_session(params, session)
-      super.tap do |user|
-        if data = session["devise.facebook_data"]        
-          user.provider = data["provider"]
-          user.uid = data["uid"]
-          user.email = data["info"]["email"] if user.email.blank?
-          user.password = Devise.friendly_token[0,20]
-          user.first_name = data["info"]["name"].split.first
-          user.last_name = data["info"]["name"].split.last
-          user.photo = data["info"]["image"]
-        end
-      end
-    end
-
-    def from_omniauth(auth)    
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
-        user.first_name = auth.info.name.split.first
-        user.last_name = auth.info.name.split.last
-        user.photo = auth.info.image      
-      end
-    end
+    
   end    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  extend FriendlyId
-  include Omniauthable
+  extend FriendlyId  
   include Filterable
   include Hashable
   include Emotionable
@@ -112,6 +111,30 @@ class User < ApplicationRecord
     area_visibility String, default: SETTING_OPTIONS.first
     name_visibility String, default: SETTING_OPTIONS.first
     children_visibility String, default: SETTING_OPTIONS.first
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"]        
+        user.provider = data["provider"]
+        user.uid = data["uid"]
+        user.email = data["info"]["email"] if user.email.blank?
+        user.password = Devise.friendly_token[0,20]
+        user.first_name = data["info"]["name"].split.first
+        user.last_name = data["info"]["name"].split.last
+        user.photo = data["info"]["image"]
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)    
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.name.split.first
+      user.last_name = auth.info.name.split.last
+      user.photo = auth.info.image      
+    end
   end
 
   # If sign in through Oauth, don't require password
