eac5b5f2ff3d1ee6d458e3804bf835ce4f3ce110
Takehiro Mouri <takehiromouri@gmail.com>
2017-02-24 19:15:32 +0545

    merge

--- app/models/group_post.rb
+++ app/models/group_post.rb
@@ -6,6 +6,8 @@ class GroupPost < ApplicationRecord
   has_many :comments, as: :post 
   has_many :post_images, as: :post
   has_many :emotions, as: :post
+  has_many :post_taggings, as: :post
+  has_many :post_tags, through: :post_taggings
   
   accepts_nested_attributes_for :post_images
 
@@ -14,6 +16,10 @@ class GroupPost < ApplicationRecord
   default_scope { order(created_at: :desc) }
 
   delegate :photo, to: :user, prefix: true
+
+  def self.tagged_with(name)
+    PostTag.find_by_name(name).group_posts.includes(:user, :post_images, :post_taggings, :post_tags)
+  end
 
   def has_likes?
     likes_count > 0
--- app/models/post_tagging.rb
+++ app/models/post_tagging.rb
@@ -1,6 +1,6 @@
 class PostTagging < ApplicationRecord
   belongs_to :post_tag
-  belongs_to :post
+  belongs_to :post, polymorphic: true, foreign_key: :post_id
   
   validates :post_id, :post_tag_id, presence: true
 end
