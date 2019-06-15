d3285183c16a3d10aeae144b799a8e36e773cbfa
Takehiro Mouri <takehiromouri@gmail.com>
2019-06-15 11:08:15 -0500

    Remove profile completed filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,40 +2,38 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-    if params[:user_search]    
-			@users = User.profile_completed
-                   .find_child_by_age_range(
+    if params[:user_search]
+			@users = User.find_child_by_age_range(
                       min: params[:user_search][:child_age_min],
                       max: params[:user_search][:child_age_max]
                     )
-                   .filter(params[:user_search].slice(:name_search, :prefecture, 
+                   .filter(params[:user_search].slice(:name_search, :prefecture,
                                                       :partner_relationship, :partner_age,
-                                                      :cancer_type, :cancer_stage))                   
+                                                      :cancer_type, :cancer_stage))
                    .uniq
-                   .paginate(page: params[:page], per_page: 10)                 
+                   .paginate(page: params[:page], per_page: 10)
                    .decorate
 		elsif params[:display_all]
-      @users = User.profile_completed                   
-                   .paginate(page: params[:page], per_page: 10)                 
+      @users = User.paginate(page: params[:page], per_page: 10)
                    .decorate
     else
-			@users = User.none.paginate(page: params[:page], per_page: 10).decorate      
+			@users = User.none.paginate(page: params[:page], per_page: 10).decorate
 		end
     @search = UserSearch.new(params[:user_search])
 	end
-    
+
   def show
     @user = User.find(params[:id]).decorate
-  end  
+  end
 
   def pending_requests
   	user = User.find(params[:user_id])
   	@requests = user.pending_requests
-  end 
+  end
 
   def friends
   	@user = User.find(params[:user_id]).decorate
   	@friends = @user.friends.paginate(page: params[:page], per_page: 20).decorate
   end
-    
+
 end
