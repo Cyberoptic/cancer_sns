82413df157d35f2faca71e05b323c482436e7f0c
Morimura Sintaro <snoopy.850807@gmail.com>
2021-03-10 11:43:01 +0800

    fix apikey

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_API_USERNAME'],
+    :user_name      => 'apikey',
     :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
