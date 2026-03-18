eaf9dd714b5ba8fdf092d9968d924ef034e6cc44
Shunsuke MIYAKE <github@helicalworks.com>
2026-03-18 15:37:46 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do  
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  # ActiveAdmin.routes(self)
   mount ActionCable.server => '/cable'
 
   authenticated :user do
